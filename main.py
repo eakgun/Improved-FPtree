3bdc51dea4f7f18c2fff990f96b945c2f098e9f0
Enes AKGUN <60859449+eakgun@users.noreply.github.com>
2021-12-01 10:11:36 +0300

    Add files via upload
    Hiearchical data transformation for plot and plotting the tree.

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from networkx.generators.classic import ladder_graph
 from fpGrowth import *
 from graphUtils import *
 import igraph as ig
+import numpy as np
 from igraph import Graph, EdgeSeq
 
 
@@ -24,8 +25,6 @@ def fpTreeFromFile(fname, minSupThreshold, minConf):
 fpTree , headerTable, itemSet = fpTreeFromFile('tesco.csv',0.1,0.2)
 
 
-# tree.add_edge(ind,self.name)
-# pos = hierarchy_pos(tree,1)
 
 fpTree.display()
 
@@ -33,21 +32,39 @@ fpTree.display()
 
 # paired_Set = [(u, v) for u in fpTree.lvld for v in fpTree.lvld if v-u == 1]  
 
-paired = list(enumerate(fpTree.lvld))
+pairs = list(enumerate(fpTree.lvld)) 
+paired = [(u,v) for v, u in pairs]
 
-paired = [(u,v) for v, u in paired]
-paired.pop(0)
 pairedx = [(v,fpTree.lbl[v]+':'+str(fpTree.cnt[v])) for u,v in paired]
 
 labeldict = {k:v for k,v in pairedx}
 
-g = ig.Graph(n=12, directed=True)
+d = defaultdict(list)
+
+for k,v in paired:
+    d[k].append(v) 
+
+new_pair = []
+
+for i in range(1, len(d)):
+    for j in range(len(d[i])):
+        for k in range(len(d[i+1])):
+            try:
+                if d[i][j] < d[i+1][k] and d[i+1][k] < d[i][j+1]:
+                    new_pair += [(d[i][j], d[i+1][k])]
+            except:
+                new_pair += [(d[i][j], d[i+1][k])]
+
+
+
+#igraph vertex IDs starts from zero.
+g = ig.Graph()
+g.add_vertices(len(pairs))
+g.add_edges(new_pair)
+layout = g.layout_reingold_tilford(root=[0])
+ig.plot(g, layout=layout)
 
 
 
 
-# Tree = nx.Graph()
-# Tree.add_edges_from(paired)
-# pos = hierarchy_pos(Tree,1)
-# nx.draw(Tree, pos=pos, labels=labeldict ,with_labels=True)
 
